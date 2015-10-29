72d53e09f2c6673808ed10afede89827fa59bb19
AgentWang007 <jaybwang007@gmail.com>
2015-10-29 17:15:49 -0400

    update migrations for paperclip implementation for curators and observations

--- db/migrate/20151029183628_add_observations.rb
+++ db/migrate/20151029183628_add_observations.rb
@@ -3,7 +3,6 @@ class AddObservations < ActiveRecord::Migration
     create_table :observations do |t|
       t.references :curator, index: true, foreign_key: true
       t.references :collection, index: true, foreign_key: true
-      t.string :image
       t.text  :description
 
       t.timestamps null: false
