9cbd9be5632509bcbba12def90c754ab4bbb77bc
AgentWang007 <jaybwang007@gmail.com>
2015-10-29 17:33:01 -0400

    add amazons3 model methods to observation

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -1,8 +1,16 @@
 class Observation < ActiveRecord::Base
-
   belongs_to :curator
   belongs_to :collection, touch: true
-
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", :storage => :s3, :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates :curator, :collection, presence: true
+
+  def s3_credentials
+    {
+     bucket: Rails.application.secrets.s3_bucket,
+     access_key_id: Rails.application.secrets.s3_key_id,
+     secret_access_key: Rails.application.secrets.s3_secret_key
+    }
+  end
 
 end
