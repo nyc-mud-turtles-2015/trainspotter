8963b0a321e238a63f0509d589d9f1cb0dc83803
AgentWang007 <jaybwang007@gmail.com>
2015-10-29 18:41:32 -0400

    update s3 credentials in secrets and model for observation

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -2,7 +2,7 @@ class Observation < ActiveRecord::Base
   belongs_to :curator
   belongs_to :collection, touch: true
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", :storage => :s3, :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   validates :curator, :collection, presence: true
 
   def s3_credentials
