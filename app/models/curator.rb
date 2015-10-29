739032d8dd84a3c3e44f34f52c978a9215fe8da4
AgentWang007 <jaybwang007@gmail.com>
2015-10-29 17:31:09 -0400

    add amazon s3 to curator model

--- app/models/curator.rb
+++ app/models/curator.rb
@@ -1,8 +1,16 @@
 class Curator < ActiveRecord::Base
   has_many :collections
   has_many :observations
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", :storage => :s3, :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  def s3_credentials
+    {
+     bucket: Rails.application.secrets.s3_bucket,
+     access_key_id: Rails.application.secrets.s3_key_id,
+     secret_access_key: Rails.application.secrets.s3_secret_key
+    }
+  end
 
 
   validates :username, presence: true
