2188e9f00c6427ec9f1c28f1ce6c82d7a8fd8ae6
Phil Sofia <phil.sofia@gmail.com>
2015-10-29 17:36:28 -0400

    Update helper files for Capybara, Shoulda Matchers, Database Cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,12 @@ require 'capybara/rspec'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
   config.include Capybara::DSL
   config.include AuthenticationHelpers
-  config.mock_with :rspec
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
   config.infer_spec_type_from_file_location!
 
   config.before(:suite) do
@@ -31,5 +34,16 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
   end
 end
