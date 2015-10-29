3651f3c8097a2854f747a7e27c5a2b05005d9deb
Phil Sofia <phil.sofia@gmail.com>
2015-10-29 17:35:21 -0400

    Create templates for controller spec files

---
+++ spec/controllers/observations_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe ObservationsController do
+  # here is some example code:
+  # let(:curator) {FactoryGirl.create(:curator)}
+  # before { @curator = FactoryGirl.create(:curator) }
+  # For difference between 'let' and 'before':
+  # https://www.launchacademy.com/codecabulary/learn-test-driven-development/rspec/before-vs-let
+
+  # To build a hash that can be used to create an object:
+  # let(:curator) { FactoryGirl.build(:curator).attributes }
+
+  # example describe block:
+  # describe "#create" do
+  #   it "creates a new curator" do
+  #     expect do
+  #       post :create, :blah => @blah
+  #     end.to change{Curator.count}.by(1)
+  #   end
+  # end
+
+
+
+
+end
