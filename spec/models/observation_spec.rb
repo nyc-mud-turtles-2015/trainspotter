ed5f60a0481c626ff63a752f1dd665d8cdb6d5e5
Phil Sofia <phil.sofia@gmail.com>
2015-10-29 18:57:29 -0400

    Fix bug by moving Observation model specs to where they belong

--- spec/models/observation_spec.rb
+++ spec/models/observation_spec.rb
@@ -1,2 +1,14 @@
 require 'rails_helper'
 
+describe Observation, type: :model do
+  context "associations" do
+    it "should belongs_to curator" do
+      observation = Observation.reflect_on_association(:curator)
+      observation.macro.should == :belongs_to
+    end
+    it "should belongs_to collection" do
+      observation = Observation.reflect_on_association(:collection)
+      observation.macro.should == :belongs_to
+    end
+  end
+end
