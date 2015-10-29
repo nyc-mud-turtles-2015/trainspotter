4b2c445e25673cb43c786e8d80529e4928faad65
AgentWang007 <jaybwang007@gmail.com>
2015-10-29 16:44:31 -0400

    update curator model for paperclip attachments

--- app/models/curator.rb
+++ app/models/curator.rb
@@ -1,6 +1,9 @@
 class Curator < ActiveRecord::Base
   has_many :collections
   has_many :observations
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
 
   validates :username, presence: true
 
