4336300c24d22c02a4fc14bb57ce26156c6648b6
Phil Sofia <phil.sofia@gmail.com>
2015-10-30 16:59:44 -0400

    Update seed file so foreign keys are not hard coded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 jason = Curator.create(name: "Jason")
 
-Collection.create(title: "Marty and Doc", description: "Great Scott!", curator_id: 5)
+collection = jason.collections.create(title: "Marty and Doc", description: "Great Scott!")
 
-Observation.create(description: "Back to the Future!", curator_id: 5, collection_id: 1, image: open("http://vignette2.wikia.nocookie.net/bttf/images/d/d2/Back_to_the_Future.jpg/revision/latest?cb=20070218045421"))
+collection.observations.create(description: "Back to the Future!", image: open("http://vignette2.wikia.nocookie.net/bttf/images/d/d2/Back_to_the_Future.jpg/revision/latest?cb=20070218045421"))
