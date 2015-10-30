67cffda0612ef18a043154479fa9c84ba13d18c0
Phil Sofia <phil.sofia@gmail.com>
2015-10-30 16:16:57 -0400

    Adjust seed file to remove observations due to photo error

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,4 @@ jason = Curator.create(name: "Jason")
 
 jason.collections.create(title: "Caffeine Trips", description: "My descent into caffeine overload", curator_id: 1)
 
-jason.observations.create(description: "I love this", curator_id:1, collection_id: 1, image: File.open(File.expand_path("../fixtures/photos/map.jpg", __FILE__)) )
+# jason.observations.create(description: "I love this", curator_id:1, collection_id: 1, image: File.open(File.expand_path("../fixtures/photos/map.jpg", __FILE__)) )
