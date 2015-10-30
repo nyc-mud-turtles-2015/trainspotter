90a6f916a0179bd813e194cac4aa2481f22c1607
Phil Sofia <phil.sofia@gmail.com>
2015-10-30 16:55:43 -0400

    Update seed and views so that new observations and seed observations are attached to curator #5

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 jason = Curator.create(name: "Jason")
 
-jason.collections.create(title: "Marty and Doc", description: "Great Scott!", curator_id: 1)
+Collection.create(title: "Marty and Doc", description: "Great Scott!", curator_id: 5)
 
-jason.observations.create(description: "Back to the Future!", curator_id:1, collection_id: 1, image: open("http://vignette2.wikia.nocookie.net/bttf/images/d/d2/Back_to_the_Future.jpg/revision/latest?cb=20070218045421"))
+Observation.create(description: "Back to the Future!", curator_id: 5, collection_id: 1, image: open("http://vignette2.wikia.nocookie.net/bttf/images/d/d2/Back_to_the_Future.jpg/revision/latest?cb=20070218045421"))
