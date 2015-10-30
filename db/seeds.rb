77286d150515330adb89a6cdae252d4fbe5df213
AgentWang007 <jaybwang007@gmail.com>
2015-10-30 17:54:12 -0400

    remove the file open from seed file so heroku won't flipm out

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,5 @@ jason = Curator.create(name: "Jason")
 nando = Collection.create(title: "Marty and Doc", description: "Great Scott!", curator_id: jason.id)
 
 
+
 Observation.create(description: "Back to the Future!", curator_id: jason.id, collection_id: nando.id)
