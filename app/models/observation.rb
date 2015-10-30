85a4c7fa0f5721af7609f0d06a9c496803fcae55
Phil Sofia <phil.sofia@gmail.com>
2015-10-30 15:55:56 -0400

    Change it back

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -1,5 +1,5 @@
 class Observation < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", :bucket  => ENV['S3_BUCKET_NAME'],
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/]
 
