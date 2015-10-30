9b041b197e846417a09c690d067e9d289398c70a
AgentWang007 <jaybwang007@gmail.com>
2015-10-30 02:21:46 -0400

    update the model to support thumb, medium, and original size

--- app/models/curator.rb
+++ app/models/curator.rb
@@ -1,6 +1,11 @@
 class Curator < ActiveRecord::Base
   has_attached_file :avatar,
-    styles: { medium: "300x300>", thumb: "100x100>" },
+    styles: {
+      :thumb => "100x100#",
+      :small  => "150x150>",
+      :medium => "200x200" },
+      :convert_options => {
+        :thumb => "-quality 75 -strip" },
     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
