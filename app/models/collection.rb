d9c1ad450c9d63bd930838ca0c0c5c2d88981142
Phil Sofia <phil.sofia@gmail.com>
2015-10-30 19:54:01 -0400

    Add pg_search to collection model

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,13 @@
 class Collection < ActiveRecord::Base
+  include PgSearch
+  pg_search_scope :search_collection, :against => {
+    :title => 'A',
+    :description => 'B'
+    },
+    :using => {
+      :tsearch => {:dictionary => "english"}
+    }
+
   belongs_to :curator
   has_many :observations
 
