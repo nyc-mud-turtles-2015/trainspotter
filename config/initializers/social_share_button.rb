2e8171c10cca961cda7198592c0c47591ce3557d
Phil Sofia <phil.sofia@gmail.com>
2015-10-31 20:25:24 -0400

    Install social_share_button gem and add all necesssary settings

---
+++ config/initializers/social_share_button.rb
@@ -0,0 +1,8 @@
+SocialShareButton.configure do |config|
+  config.allow_sites = %w(twitter facebook google_plus email)
+end
+
+# This is all of the possible options:
+# SocialShareButton.configure do |config|
+#   config.allow_sites = %w(twitter facebook google_plus weibo douban tqq renren qq kaixin001 baidu tumblr plurk pinterest email)
+# end
