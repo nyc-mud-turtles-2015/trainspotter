b258996edfd5e5dd75955e070a533417966446c4
Maggie Baker <maggiedbaker@gmail.com>
2015-11-01 13:11:03 -0500

    set curator-actions-controller back to the way it was

--- app/controllers/curator_actions_controller.rb
+++ app/controllers/curator_actions_controller.rb
@@ -1,10 +1,10 @@
 class CuratorActionsController < ApplicationController
 
-  before_action :not_logged_in?, only: [:new, :create, :destroy]
+  before_action :logged_in?, only: [:new, :create, :destroy]
 
   private
 
-  def not_logged_in?
+  def logged_in?
     unless current_user
       redirect_to login_path
     end
