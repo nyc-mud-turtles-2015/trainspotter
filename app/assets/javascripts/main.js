3c06dc40c40c080713d4dfcc2d8c7f810baf0197
Phil Sofia <phil.sofia@gmail.com>
2015-11-01 19:58:54 -0500

    Comment out all javascript to build everything before implementing ajax

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,20 +1,21 @@
-$( document ).ready(function() {
-  $('#curators_search').submit(function(event) {
-   event.preventDefault();
-    $.ajax({
-      type: "GET",
-      url:  "/curators/search",
-      data: $('#curators_search').serialize(),
-      success: function(data) {
-        console.log("success");
-        $('#append_curator_search_results').append(data);
-      },
-      error: function() {
-         console.log("didnt work");
-      }
-    });
-  });
-});
+// $( document ).ready(function() {
+//   $('#curators_search').submit(function(event) {
+//    event.preventDefault();
+//    // debugger;
+//     $.ajax({
+//       type: "GET",
+//       url:  "/curators/search",
+//       data: $('#curators_search').serialize(),
+//       success: function(data) {
+//         console.log("success");
+//         $('#append_curator_search_results').append(data);
+//       },
+//       error: function() {
+//          console.log("didnt work");
+//       }
+//     });
+//   });
+// });
 
 
 
