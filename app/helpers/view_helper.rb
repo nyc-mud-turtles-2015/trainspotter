8c5d2da003063969966efae1320ed6b804b1bdcc
Phil Sofia <phil.sofia@gmail.com>
2015-11-03 11:35:25 -0500

    Add two view helper methods for thumb image existence and path name for show_all view

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -1,7 +1,18 @@
 module ViewHelper
-
   def belongs_to_current_user?(object)
     current_user == object.curator
   end
 
+  def collection_image?(coll)
+    if coll.approved_observations.last
+      if coll.approved_observations.last.image_file_name.present?
+        return true
+      end
+    end
+    return false
+  end
+
+  def collection_thumb_image(coll)
+    coll.observations.last.image.url(:thumb)
+  end
 end
