3a45ec414163804de724367da4d53b639bd134ef
Maggie Baker <maggiedbaker@gmail.com>
2015-11-03 17:04:18 -0500

    Refactor curator actions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :redirect_if_not_logged_in, only: [:new, :create, :destroy]
+
   helper_method :current_user, :logged_in?
 
   private
@@ -13,6 +15,15 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     current_user != nil
+
+  def redirect_if_not_logged_in
+    unless current_user
+      redirect_to login_path
+    end
   end
 
 end
+
+
+
+
