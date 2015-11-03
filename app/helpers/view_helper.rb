b3211f8f6f9b8913270dded3e5ecf7b2626dc797
Phil Sofia <phil.sofia@gmail.com>
2015-11-02 20:45:45 -0500

    Remove from view helper three methods, which were moved to collections model

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -4,16 +4,4 @@ module ViewHelper
     current_user == object.curator
   end
 
-  def has_pending_observations?(collection)
-    !collection.pending_observations.empty?
-  end
-
-  def has_approved_observations?(collection)
-    !collection.approved_observations.empty?
-  end
-
-  def current_user_can_add?(collection)
-    current_user.can_create?(collection) || collection.owned_by?(current_user) || current_user.admin?(collection)
-  end
-
 end
