6e21fed5237404b967f9adf378281e3a75cfe4bd
Phil Sofia <phil.sofia@gmail.com>
2015-11-04 14:51:26 -0500

    Add SweetAlerts to javascript delete alerts functionality

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,13 +1,35 @@
+function loadUrl(location)
+{
+  this.document.location.href = location;
+}
+
+function loadDoc(url) {
+    $.ajax({
+      url: url,
+      type: 'delete'
+    })
+    .done(function(response) {
+      $(document).innerHTML = response;
+    })
+    .fail(function(){
+    });
+}
+
 $( document ).ready(function() {
   $("#delete_collection").click(function(event) {
+    var delete_url = $('#delete_collection a').attr('href');
+    var curator_profile = $('#js_owner_of_collection a').attr('href');
     event.preventDefault();
-    var r = confirm("Are you sure you want to delete this collection?");
-    if (r === true) {
-        alert("Collection has been deleted.");
-    } else {
-      location.reload();
-      alert("Collection has NOT been deleted.");
-      return false;
-    }
+    swal({   title: "Are you sure you want to delete this collection?",   text: "You will not be able to recover this collection!",   type: "warning",   showCancelButton: true,   confirmButtonColor: "#DD6B55",   confirmButtonText: "Yes, delete it!",   cancelButtonText: "No, cancel!",   closeOnConfirm: false,   closeOnCancel: false }, function(isConfirm){
+        if (isConfirm) {
+          loadDoc(delete_url);
+          swal({   title: "Your collection has been deleted!",   text: "Just a second!",   timer: 2000,   showConfirmButton: false });
+          setTimeout(function(){ loadUrl(curator_profile); }, 1000);
+        }
+        else {
+          swal("Cancelled", "Your collection is safe :)", "error");
+        }
+      });
+    return false;
   });
 });
