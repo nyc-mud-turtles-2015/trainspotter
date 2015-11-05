e5094380bee25c46e988a0904095a0233153a8bd
AgentWang007 <jaybwang007@gmail.com>
2015-11-04 20:44:18 -0500

    fix butch

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -4,7 +4,6 @@ class Auth0Controller < ApplicationController
     curator = Curator.find_or_create_by(uid: request.env['omniauth.auth']['uid'])
     #Note: the name attribute is being updated properly, but the avatar attribute is not being updated properly
     curator.update_attributes(avatar: request.env['omniauth.auth']['info']['image'], name: request.env['omniauth.auth']['info']['name'])
-        curator.username = Faker::Internet.user_name('butch')
 
     curator.save
     session[:uid] = curator.uid
